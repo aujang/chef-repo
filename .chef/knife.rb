c08acc7a26a6465d337e49ec38e99790432b4322
Aujang Abadi <aujang@srrngames.com>
2014-08-17 14:55:28 -0400

    Hosted chef knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,13 @@
-cookbook_path [ '/root/chef-repo/cookbooks' ]
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "spectheintro"
+client_key               "#{current_dir}/spectheintro.pem"
+validation_client_name   "evenspring-validator"
+validation_key           "#{current_dir}/evenspring-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/evenspring"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
